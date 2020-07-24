83bee564bb743e43fcedcd5d2242d64821714d9a
pietdevries94 <piet@devries.tech>
2020-07-24 13:02:05 +0200

    fix import

--- default.nix
+++ default.nix
@@ -1,11 +1,12 @@
 { system ? builtins.currentSystem }:
 let
   pkgs = import <nixpkgs> { inherit system; };
+  self = in self
 in rec {
-  vscode = pkgs.callPackage ./pkgs/applications/vscode/vscode {
+  vscode = pkgs.callPackage ./pkgs/applications/vscode/vscode.nix {
     inherit pkgs;
   };
-  vscodium = pkgs.callPackage ./pkgs/applications/vscode/vscodium {
+  vscodium = pkgs.callPackage ./pkgs/applications/vscode/vscodium.nix {
     inherit pkgs;
   };
 }
