a785f38a9bd7ab246cc3e47b41c147382723920d
pietdevries94 <piet@devries.tech>
2020-07-24 12:53:26 +0200

    Attempt 1

--- default.nix
+++ default.nix
@@ -1,7 +1,6 @@
 { system ? builtins.currentSystem }:
 let
   pkgs = import <nixpkgs> { inherit system; };
-  self = in self;
 in rec {
   vscode = pkgs.callPackage ./pkgs/applications/vscode/vscode {
     inherit pkgs;
