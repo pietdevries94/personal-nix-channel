48e6acf266e82e09308af7a2bdf36fe208a3d353
pietdevries94 <pietdevries94@users.noreply.github.com>
2020-09-07 23:13:00 +0000

    Apply automatic updates

--- pkgs/development/nix-boilerplate/fetch.nix
+++ pkgs/development/nix-boilerplate/fetch.nix
@@ -6,5 +6,5 @@ in
     repo = "nix-boilerplate";
     rev = "c9343f83ab0d38ec94912e8a04f3c807a36d90f5";
     sha256 = "0r6fvizqf0hfaa22xqxxxsra63g368dp5vf6gxd018yxwxkvyqnw";
-    fetchSubmodules = false;
+    fetchSubmodules = true;
   }
