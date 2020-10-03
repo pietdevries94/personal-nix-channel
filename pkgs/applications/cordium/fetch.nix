fa35a00fefbd3614c02bbd44ad99c3e9f99054ee
pietdevries94 <pietdevries94@users.noreply.github.com>
2020-10-03 19:14:08 +0000

    Apply automatic updates

--- pkgs/applications/cordium/fetch.nix
+++ pkgs/applications/cordium/fetch.nix
@@ -6,4 +6,5 @@ in
     repo = "Cordium";
     rev = "100c5cb0c1d57df02523622b7833c08b22d511f2";
     sha256 = "1x24xlda7bpqg3bbyphd58szbw04hxbhihnwf2kw3hglp8f724c5";
+    fetchSubmodules = true;
   }
