6f6b2a57582e4b70327d60926879b37e1e49c296
pietdevries94 <pietdevries94@users.noreply.github.com>
2020-10-03 18:16:16 +0000

    Apply automatic updates

--- pkgs/applications/cordium/fetch.nix
+++ pkgs/applications/cordium/fetch.nix
@@ -4,7 +4,7 @@ in
   pkgs.fetchFromGitHub {
     owner = "pietdevries94";
     repo = "Cordium";
-    rev = "842f581d316e25cd91f4bc5caabe94630b9bc7df";
-    sha256 = "1krrqg774g1v5ix3bpq0zg1bw2pmk6prjczch4k3j4wiqxfp330m";
+    rev = "c72c90105719efae51ccb775a9203250ff1b06f8";
+    sha256 = "0yn3zkk9wfgzlncdha3xzrc1757fvrppkpi0dbqssxpbnkpdngf0";
     fetchSubmodules = true;
   }
