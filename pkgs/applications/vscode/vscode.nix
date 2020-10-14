16f2c996d5221d099006c619d48e7d2fa6ca875c
pietdevries94 <pietdevries94@users.noreply.github.com>
2020-10-14 17:16:24 +0000

    Apply automatic updates

--- pkgs/applications/vscode/vscode.nix
+++ pkgs/applications/vscode/vscode.nix
@@ -11,8 +11,8 @@ let
   archive_fmt = if system == "x86_64-darwin" then "zip" else "tar.gz";
 
   sha256 = {
-    x86_64-linux = "12nrv037an4f6h8hrbmysc0lk5wm492hywa7lp64n4d308zg5567";
-    x86_64-darwin = "1z22hn2ngx2x5l9h6zsblpyzr85lyjzv2ayplscbgaa9ff52l429";
+    x86_64-linux = "";
+    x86_64-darwin = "";
   }.${system};
 in
   callPackage ./generic.nix rec {
@@ -21,7 +21,7 @@ in
 
     # Please backport all compatible updates to the stable release.
     # This is important for the extension ecosystem.
-    version = "1.50.0";
+    version = "";
     pname = "vscode";
 
     executableName = "code" + lib.optionalString isInsiders "-insiders";
