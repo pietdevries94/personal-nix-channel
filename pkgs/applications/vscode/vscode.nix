22f31b6e0b2b1bf82544732a028a0d4d8697404d
pietdevries94 <pietdevries94@users.noreply.github.com>
2021-01-12 05:42:50 +0000

    Apply automatic updates

--- pkgs/applications/vscode/vscode.nix
+++ pkgs/applications/vscode/vscode.nix
@@ -12,7 +12,7 @@ let
 
   sha256 = {
     x86_64-linux = "";
-    x86_64-darwin = "1qgadm52c5lzkvgvqrz0n8brm4qbjg8hf1dk6a2ynqhqjxcwbj4r";
+    x86_64-darwin = "";
   }.${system};
 in
   callPackage ./generic.nix rec {
@@ -21,7 +21,7 @@ in
 
     # Please backport all compatible updates to the stable release.
     # This is important for the extension ecosystem.
-    version = "1.52.1";
+    version = "";
     pname = "vscode";
 
     executableName = "code" + lib.optionalString isInsiders "-insiders";
