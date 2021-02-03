6fe9c98a830ee6acff5d3c723d25b81d29405361
pietdevries94 <pietdevries94@users.noreply.github.com>
2021-02-03 17:37:28 +0000

    Apply automatic updates

--- pkgs/applications/vscode/vscode.nix
+++ pkgs/applications/vscode/vscode.nix
@@ -21,7 +21,7 @@ in
 
     # Please backport all compatible updates to the stable release.
     # This is important for the extension ecosystem.
-    version = "";
+    version = "1.52.1";
     pname = "vscode";
 
     executableName = "code" + lib.optionalString isInsiders "-insiders";
