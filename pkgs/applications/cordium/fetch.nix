72a0ba1db5c3e76fcb8f59692ae07cc4009497dd
pietdevries94 <pietdevries94@users.noreply.github.com>
2021-11-01 22:14:06 +0000

    Apply automatic updates

--- pkgs/applications/cordium/fetch.nix
+++ pkgs/applications/cordium/fetch.nix
@@ -5,6 +5,6 @@ in
     owner = "pietdevries94";
     repo = "Cordium";
     rev = "bebc48f4e559822255f203d618c03a114f21b802";
-    sha256 = "04rwa00yj1f2g90wa8dk4jc54q3mkn302i9jlx56d6anibidkd9p";
+    sha256 = "N7XZ4opWmWZKpzJFAYaddWBSmCSzIcVBesIF6QFQPBM=";
     fetchSubmodules = true;
   }
--- pkgs/development/nix-boilerplate/fetch.nix
+++ pkgs/development/nix-boilerplate/fetch.nix
@@ -5,6 +5,6 @@ in
     owner = "pietdevries94";
     repo = "nix-boilerplate";
     rev = "af6f28e4dfbaa8a5baabefdee49d050a102a78a3";
-    sha256 = "06ipxnwikwc7xkdb9yyknj6ggz0x0nk7dkjz9w5qw5b3acx5csc9";
+    sha256 = "iWlWOlNjFY4LT1/OdqYFHfz3jLTT+7Ta7IfxGbntNxo=";
     fetchSubmodules = true;
   }
