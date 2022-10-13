e44899a2c90213ba495063b660a88b64ee1658ff
pietdevries94 <pietdevries94@users.noreply.github.com>
2022-10-13 18:31:51 +0000

    Apply automatic updates

--- pkgs/applications/vscode/vscodium.nix
+++ pkgs/applications/vscode/vscodium.nix
@@ -27,7 +27,7 @@ in
 
     # Please backport all compatible updates to the stable release.
     # This is important for the extension ecosystem.
-    version = "1.72.1.22284";
+    version = "1.72.2.22286";
     pname = "vscodium";
 
     executableName = "codium";
