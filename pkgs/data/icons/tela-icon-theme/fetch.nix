81ff28a02414e6c46d5a965a07cfd621c2b181e7
pietdevries94 <pietdevries94@users.noreply.github.com>
2023-02-02 18:19:57 +0000

    Apply automatic updates

--- pkgs/data/icons/tela-icon-theme/fetch.nix
+++ pkgs/data/icons/tela-icon-theme/fetch.nix
@@ -4,7 +4,7 @@ in
   pkgs.fetchFromGitHub {
     owner = "vinceliuice";
     repo = "Tela-icon-theme";
-    rev = "1389a68c894fec1f5798fe8b76edf77fb4ab1528";
-    sha256 = "SW4MNvtrHpJm6yiS9k11XzzzGSMXdZ71cnvASK1dzno=";
+    rev = "db7dde863def743afd6ba26a4c1ecd6dccd1ff08";
+    sha256 = "W3gGn4ioTg/iOWTa5hfwx6FWzpFilpG8IXwaO5/YAvk=";
   }
 
